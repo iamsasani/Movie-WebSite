77eaeb3e654df5be82713967c5b02b05def25695
iamsasani <218357133+iamsasani@users.noreply.github.com>
2026-09-08 13:31:03 +0330

    add data file

---
+++ src/data/data.jsx
@@ -0,0 +1,7 @@
+
+export const ApiKey =import.meta.env.VITE_TMDB_API_KEY;
+
+// popular movie
+export const BaseUrlMovie = "https://api.themoviedb.org/3";
+
+export const BaseUrlImage = "https://image.tmdb.org/t/p";
